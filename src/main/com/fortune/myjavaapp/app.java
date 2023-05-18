9b8ba4d083b90a113c7047b9095f1a855a4195be
satyajit7057 <satyajitdeshmukh371@gmail.com>
2023-05-18 15:47:18 +0000

    first

---
+++ src/main/com/fortune/myjavaapp/app.java
@@ -0,0 +1,14 @@
+package com.fortune.myjavaapp;
+/**
+ * Hello world!
+ */
+public class app {
+    private static final String MESSAGE = "Hello World!";
+    public app() {}
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
